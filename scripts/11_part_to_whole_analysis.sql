c33cd876d5f5f2fe65c39843a01e5e1794706a79
bharatheshagouda-cs <bharatheshagoudacs@gmail.com>
2025-10-12 23:05:00 +0530

    Update 11_part_to_whole_analysis.sql

--- scripts/11_part_to_whole_analysis.sql
+++ scripts/11_part_to_whole_analysis.sql
@@ -28,4 +28,5 @@ SELECT
     SUM(total_sales) OVER () AS overall_sales,
     ROUND((CAST(total_sales AS FLOAT) / SUM(total_sales) OVER ()) * 100, 2) AS percentage_of_total
 FROM category_sales
+
 ORDER BY total_sales DESC;
