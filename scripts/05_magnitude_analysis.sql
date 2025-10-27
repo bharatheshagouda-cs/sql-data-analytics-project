184f68394d7ef00382f15de107f45307d4595d25
bharatheshagouda-cs <bharatheshagoudacs@gmail.com>
2025-10-27 23:19:49 +0530

    Update 05_magnitude_analysis.sql

--- scripts/05_magnitude_analysis.sql
+++ scripts/05_magnitude_analysis.sql
@@ -5,7 +5,7 @@ Magnitude Analysis
 Purpose:
     - To quantify data and group results by specific dimensions.
     - For understanding data distribution across categories.
-
+    hhfhfdfv     
 SQL Functions Used:
     - Aggregate Functions: SUM(), COUNT(), AVG()
     - GROUP BY, ORDER BY
