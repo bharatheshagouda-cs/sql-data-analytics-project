aafe69c02f4580f3602b2bc80169aa4be69bbde9
bharatheshagouda-cs <bharatheshagoudacs@gmail.com>
2025-11-05 22:07:31 +0530

    Update 08_cumulative_analysis.sql

--- scripts/08_cumulative_analysis.sql
+++ scripts/08_cumulative_analysis.sql
@@ -10,7 +10,7 @@ Purpose:
 SQL Functions Used:
     - Window Functions: SUM() OVER(), AVG() OVER()
 ===============================================================================
-*/ djdjdjdjd    
+*/
 
 -- Calculate the total sales per month 
 -- and the running total of sales over time 
@@ -19,9 +19,9 @@ SELECT
 	total_sales,
 	SUM(total_sales) OVER (ORDER BY order_date) AS running_total_sales,
 	AVG(avg_price) OVER (ORDER BY order_date) AS moving_average_price
-FROM sjdjdd dndjd
+FROM
 (
-    SELECT jsjd   xnxjdjdj
+    SELECT
         DATETRUNC(year, order_date) AS order_date,
         SUM(sales_amount) AS total_sales,
         AVG(price) AS avg_price
