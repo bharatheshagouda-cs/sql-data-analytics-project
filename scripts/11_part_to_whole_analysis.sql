e8f9fc0fa8bd65061dd8b4f7a5c05d0ed01e87f2
bharatheshagouda-cs <bharatheshagoudacs@gmail.com>
2025-11-07 20:06:14 +0530

    Update 11_part_to_whole_analysis.sql

--- scripts/11_part_to_whole_analysis.sql
+++ scripts/11_part_to_whole_analysis.sql
@@ -5,11 +5,11 @@ Part-to-Whole Analysis
 Purpose:
     - To compare performance or metrics across dimensions or time periods.
     - To evaluate differences between categories.
-    - Useful for A/B testing or regional comparisons.
+    - Useful for A/B testing or regional comparisons. fhxhdjd    d
 
 SQL Functions Used:
     - SUM(), AVG(): Aggregates values for comparison.
-    - Window Functions: SUM() OVER() for total calculations.
+    - Window Functions: SUM() OVER() for total calculations. jdjdjdjd  jdjdj
 ===============================================================================
 */
 -- Which categories contribute the most to overall sales?
