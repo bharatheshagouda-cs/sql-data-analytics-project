0e12d23b1c4a2aaebdf2d30bfca0359965d6ea4d
bharatheshagouda-cs <bharatheshagoudacs@gmail.com>
2025-11-27 23:35:00 +0530

    Update 10_data_segmentation.sql

--- scripts/10_data_segmentation.sql
+++ scripts/10_data_segmentation.sql
@@ -11,7 +11,7 @@ SQL Functions Used:
     - GROUP BY: Groups data into segments.
 ===============================================================================
 */
-
+ghjjghh hddg vg
 /*Segment products into cost ranges and 
 count how many products fall into each segment*/
 WITH product_segments AS (
@@ -26,14 +26,14 @@ WITH product_segments AS (
             ELSE 'Above 1000'
         END AS cost_range
     FROM gold.dim_products
-)
+)hdhhbfgv bchb ggjv
 SELECT 
     cost_range,
     COUNT(product_key) AS total_products
 FROM product_segments
 GROUP BY cost_range
 ORDER BY total_products DESC;
-
+vsghch gfhb gffh
 /*Group customers into three segments based on their spending behavior:
 	- VIP: Customers with at least 12 months of history and spending more than €5,000.
 	- Regular: Customers with at least 12 months of history but spending €5,000 or less.
