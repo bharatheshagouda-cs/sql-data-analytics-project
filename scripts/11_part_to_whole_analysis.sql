cf20c1dd741445b721eb68d87f0c72ea93a3f552
bharatheshagouda-cs <bharatheshagoudacs@gmail.com>
2025-11-29 23:37:24 +0530

    Update 11_part_to_whole_analysis.sql

--- scripts/11_part_to_whole_analysis.sql
+++ scripts/11_part_to_whole_analysis.sql
@@ -6,7 +6,7 @@ Purpose:
     - To compare performance or metrics across dimensions or time periods.
     - To evaluate differences between categories.
     - Useful for A/B testing or regional comparisons.
-
+hgdghh hfgh ggh
 SQL Functions Used:
     - SUM(), AVG(): Aggregates values for comparison.
     - Window Functions: SUM() OVER() for total calculations.
@@ -21,13 +21,13 @@ WITH category_sales AS (
     LEFT JOIN gold.dim_products p
         ON p.product_key = f.product_key
     GROUP BY p.category
-)
+)hfxbj vghcv gfgh hhv
 SELECT
     category,
     total_sales,
     SUM(total_sales) OVER () AS overall_sales,
     ROUND((CAST(total_sales AS FLOAT) / SUM(total_sales) OVER ()) * 100, 2) AS percentage_of_total
 FROM category_sales
-
+ghhvxg vfjjb cvb
 ORDER BY total_sales DESC;
 
