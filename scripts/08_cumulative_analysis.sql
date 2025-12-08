515660051c9f222def9409470a3fbaad539e5fc6
bharatheshagouda-cs <bharatheshagoudacs@gmail.com>
2025-12-08 20:20:03 +0530

    Update 08_cumulative_analysis.sql

--- scripts/08_cumulative_analysis.sql
+++ scripts/08_cumulative_analysis.sql
@@ -20,7 +20,7 @@ SELECT
 	SUM(total_sales) OVER (ORDER BY order_date) AS running_total_sales,
 	AVG(avg_price) OVER (ORDER BY order_date) AS moving_average_price
 FROM
-(hxhxhch jfjf jfyf
+(
     SELECT
         DATETRUNC(year, order_date) AS order_date,
         SUM(sales_amount) AS total_sales,
